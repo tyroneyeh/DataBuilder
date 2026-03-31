9f3550611e2bc3f7f689cf5ca576883bf966de25
Tyrone Yeh <siryeh@gmail.com>
2026-03-31 22:00:37 +0800

    Added conditional check for GET requests in fetch event listener

--- sw.js
+++ sw.js
@@ -10,6 +10,9 @@ self.addEventListener('install', (event) => {
 })
 
 self.addEventListener('fetch', (event) => {
+    if (event.request.method !== 'GET') {
+        return
+    }
     event.respondWith(
         fetch(event.request)
             .then((response) => {
