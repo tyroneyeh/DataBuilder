6aa5786945c9b4d9671078a1af17d518c289a189
Tyrone Yeh <siryeh@gmail.com>
2026-04-04 11:04:38 +0800

    Updated to use Workbox for service worker updates and added translation for "A new version is available. Do you want to reload?"

--- sw.js
+++ sw.js
@@ -1,15 +1,39 @@
 const CACHE_NAME = 'ty-db-v1'
 
-self.addEventListener('install', (event) => {
+globalThis.addEventListener('message', (event) => {
+    if (event.origin !== globalThis.location.origin) {
+        console.warn('Received message from unexpected origin', event.origin)
+        return
+    }
+    if (event.data?.type === 'SKIP_WAITING') {
+        globalThis.skipWaiting()
+    }
+})
+
+globalThis.addEventListener('install', (event) => {
     event.waitUntil(
         caches.open(CACHE_NAME).then((cache) => {
             return cache.addAll(['./', './index.html'])
         })
     )
-    self.skipWaiting()
 })
 
-self.addEventListener('fetch', (event) => {
+globalThis.addEventListener('activate', (event) => {
+    event.waitUntil(
+        caches.keys().then((cacheNames) => {
+            return Promise.all(
+                cacheNames.map((cacheName) => {
+                    if (cacheName !== CACHE_NAME) {
+                        console.log('Deleting old cache', cacheName);
+                        return caches.delete(cacheName);
+                    }
+                })
+            )
+        })
+    )
+})
+
+globalThis.addEventListener('fetch', (event) => {
     if (event.request.method !== 'GET') {
         return
     }
